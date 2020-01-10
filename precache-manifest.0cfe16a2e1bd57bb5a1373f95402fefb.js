dc2df3bc22e7012dd55a7912befe98ed9d2113a1
Juan David <jdospin@hotmail.com>
2020-01-10 13:15:35 -0500

    Updates

---
+++ precache-manifest.0cfe16a2e1bd57bb5a1373f95402fefb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2004b6fc0e871b784d2f430d53a467ce",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "2da567b79db6e4ee857b",
+    "url": "/robofriends/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "72f07d37a57e5d51cb72",
+    "url": "/robofriends/static/css/main.64df9dfe.chunk.css"
+  },
+  {
+    "revision": "2da567b79db6e4ee857b",
+    "url": "/robofriends/static/js/2.790cee96.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/robofriends/static/js/2.790cee96.chunk.js.LICENSE"
+  },
+  {
+    "revision": "72f07d37a57e5d51cb72",
+    "url": "/robofriends/static/js/main.f7b37ed8.chunk.js"
+  },
+  {
+    "revision": "0d000ca805ddaefdf0fc",
+    "url": "/robofriends/static/js/runtime-main.8ebeaa0d.js"
+  }
+]);
